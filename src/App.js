d51a772d1e26d18222b9e0f92727a86bc27b0bb6
David Okolo <david.okolo@icloud.com>
2019-12-28 17:57:47 +0100

    Understanding JSX under the hood

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // import the React class. Fact: even in a jsx representation where it was never actually called, it is needed because when the jsx is being transpiled by our build tool to the normal js repesentation it need the React class
+import './App.css'; // the accompanying css
 
+// This is a functional component (stateless component)
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // React.createElement is the method that is run when the jsx is being compiled. JSX is simply syntactic sugar
+  const titleHeading = React.createElement('h1', null, 'Welcome to react!!!')
+  return React.createElement('div', {className: 'App'}, titleHeading)
 }
 
+// ES6 style function expression
+const jsxApp = () => {
+
+  // JSX representation
+  return (
+    <div className='App'>
+      <h1>Welcome to react!!!</h1>
+    </div>
+  )
+}
+
+// primarily export the App function out of this file
 export default App;
